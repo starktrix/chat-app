cfc5fd998b4f4d125efceec41a87f230dd46e5de
starktrix <82788294+starktrix@users.noreply.github.com>
2025-12-26 08:16:06 +0000

    login persistent user notify toast removed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,10 +17,10 @@ function Login() {
     });
   };
 
-  useEffect(() => {
-    notify('user1', 'user1@gmail.com', 'TestUser1@0');
-    notify('user2', 'user2@gmail.com', 'TestUser2@0');
-  }, []);
+  // useEffect(() => {
+  //   notify('user1', 'user1@gmail.com', 'TestUser1@0');
+  //   notify('user2', 'user2@gmail.com', 'TestUser2@0');
+  // }, []);
 
   return (
     <>
